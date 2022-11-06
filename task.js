77aee1bb758c0fb4c700a80ec7c2d56fd0a7e0c2
Aznavyr <cesar-88@mail.ru>
2022-11-06 15:28:15 +0300

    Fixed

--- task.js
+++ task.js
@@ -1,21 +1,17 @@
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discriminant = Math.pow(b, 2) - 4 * a * c;
-  if(discriminant < 0){
-    let arr = 0; 
-    return(arr);
-  }else if(discriminant == 0){
+  const arr = [];
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  
+  if(discriminant == 0){
     let root = -b / (2 * a);
     arr[0] = Number(root.toFixed(2));
-    return(arr)
   }else if(discriminant > 0){
     let root1 = (-b + Math.sqrt(discriminant) )/(2*a);
     let root2 = (-b - Math.sqrt(discriminant) )/(2*a);
-    arr[0] = Number(root1.toFixed(2));
-    arr[1] = Number(root2.toFixed(2));
-    return( arr);
+    arr[0] = root1;
+    arr[1] = root2;
   }
-   return arr; // array
+   return arr;
 }
 
 
